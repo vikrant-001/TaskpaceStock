1e968f2ba8daa46dad67b9baeb03cd320429447a
vikrant-001 <121452566+vikrant-001@users.noreply.github.com>
2023-12-20 12:33:23 +0530

    final commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,48 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import classes from './page.module.css';
+import Intro from '../../components/Intro/Name';
+import Aboutme from '../../components/Aboutme/Aboutme'
+import Experience from '../../components/experinces/Experience';
+import Project from '../../components/project/Project';
+import LeaderShip from '../../components/LeaderShip/LeaderShip';
+import Link from 'next/link';
+import Skills from '../../components/Skills/Skills';
+import Footer from '../../components/footer/Footer';
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  return <div className={classes.main}>
+    {/* nav bar for navigation */}
+    <nav className={classes.navbar}>
+        <p><Link className={classes.linksc} href="#intro">{`<vikrant/>`}</Link></p>
+        <p> <Link className={classes.linksc} href='#proj'>Projects</Link></p>
+        <p> <Link className={classes.linksc} href='#res'>Resume</Link></p>
+        <p><Link className={classes.linksc} href='#about'>About</Link></p>
+        <p><Link className={classes.linksc} href='#skill'>Skills</Link></p>
+    </nav>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <div id='intro'>
+        <Intro/>
+    </div>
+    <div id='about'>
+        <Aboutme/>
+    </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+    <div id = 'exp'>
+        <Experience/>
+    </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    <div id='proj'>
+      <Project/> 
+    </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+    <div >
+        <LeaderShip/>
+    </div>
+    <div id='skill'>
+        <Skills/>
+    </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div>
+        <Footer/>
+    </div>
+  
+  </div>
 }
